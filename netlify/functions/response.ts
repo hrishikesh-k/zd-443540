bc4fd3350586ff038634ade2cb2d4c052a31a94d
Hrishikesh Kokate <hrishi5200@gmail.com>
2025-10-23 00:58:16 +0800

    first commit

---
+++ netlify/functions/response.ts
@@ -0,0 +1,11 @@
+import type {Config} from '@netlify/functions'
+
+export default async function handler() {
+  return Response.json({
+    message: 'WAF bypass successful'
+  })
+}
+
+export const config: Config = {
+  path: '/response'
+}
---
+++ netlify/functions/waf-bypass.ts
@@ -0,0 +1,15 @@
+import type {Config} from '@netlify/functions'
+
+export default async function handler(req: Request) {
+  const res = await fetch('https://zd-443540.netlify.app/response', {
+    headers: {
+      'x-nf-waf-bypass': req.headers.get('x-nf-waf-bypass') || '',
+    }
+  })
+  const json = await res.json()
+  return Response.json(json)
+}
+
+export const config: Config = {
+  path: '/waf-bypass'
+}
